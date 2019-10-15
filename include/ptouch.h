f6aadef2f84f3063517d7901d868aea0bdcd1675
Jarl Gullberg <gullberg.jarl@fmv.se>
2019-10-15 10:12:29 +0200

    Strip spurious whitespace.

--- include/ptouch.h
+++ include/ptouch.h
@@ -1,17 +1,17 @@
 /*
 	ptouch-print - Print labels with images or text on a Brother P-Touch
-	
+
 	Copyright (C) 2015-2019 Dominic Radermacher <blip@mockmoon-cybernetics.ch>
 
 	This program is free software; you can redistribute it and/or modify it
 	under the terms of the GNU General Public License version 3 as
 	published by the Free Software Foundation
-	
+
 	This program is distributed in the hope that it will be useful, but
 	WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 	See the GNU General Public License for more details.
-	
+
 	You should have received a copy of the GNU General Public License
 	along with this program; if not, write to the Free Software Foundation,
 	Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
